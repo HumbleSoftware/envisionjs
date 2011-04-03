49e5a41481650f8b6b75bd99056573a2be28a81f
Carl Sutherland <carl@humblesoftware.com>
2011-04-03 17:05:51 -0400

    Initial general bubble implementation - animates along a time series.

--- js/HumbleBubble.js
+++ js/HumbleBubble.js
@@ -10,73 +10,53 @@ if (typeof(Humble) == 'undefined') {
  * @author Carl Sutherland
  * @version 1.0.0
  */
-Humble.Bubble = function () {this.init.apply(this, arguments)};
-Humble.Bubble.prototype = {
+(function () { 
 
-    init : function (id) {
+    var Bubble = function (options) {
 
-        this.id = id;
-
-        var d1 = [],
-            d2 = [],
-            point;
-            
-        for (var i = 0; i < 10; i++) {
-            point = [Math.ceil(Math.random()*10), Math.ceil(Math.random()*10), Math.ceil(Math.random()*10)];
-            d1.push(point);
-            point = [Math.ceil(Math.random()*10), Math.ceil(Math.random()*10), Math.ceil(Math.random()*10)];
-            d2.push(point);
-        }
-
-        this.d1 = d1;
-        this.d2 = d2;
-    }, 
-    
-    draw : function () {
-
-        var that = this,
-            timeout = null,
-            i = 1;
-
-        var localDraw = function () {
-            that.drawBubble.apply(that, [i]);
-            i++;
-            if (i < 100) {
-                if (timeout) clearTimeout(timeout);
-                timeout = setTimeout(localDraw, 25);
-            }
-        }
-
-        localDraw();
-    },
-
-    drawBubble : function (i) {
-
-        var d1 = this.d1,
-            d2 = this.d2,
-            d  = [];
-
-        for (var j = 0; j < 10; j++) {
-
-            var a = d1[j][0] + (d2[j][0] - d1[j][0]) * (1 - i/100),
-                b = d1[j][1] + (d2[j][1] - d1[j][1]) * (1 - i/100),
-                c = d1[j][2] + (d2[j][2] - d1[j][2]) * (1 - i/100),
-                point;
-
-            point = [a, b, c];
-
-            d.push([point]);
-        }
-
-        Flotr.draw(
-            $(this.id),
-            d,
-            {
-                bubbles : { show : true, baseRadius : 5 },
-                grid: {outlineWidth: 0, labelMargin: 0},
-                xaxis   : { min : 0, max : 10, ticks : null, noTicks : 10, tickFormatter : function () { return ''; } },
-                yaxis   : { min : 0, max : 10, ticks : null, noTicks : 10, tickFormatter : function () { return ''; } }
-            }
-        );
+        this.node       = $(options.node);
+        this.data       = options.data;
+        this.animate    = options.animate;
     }
-};
+
+    Bubble.prototype = {
+
+        draw : function () {
+
+            var data = this.data,
+                that = this,
+                i    = 0,
+                timeout;
+
+            var localDraw = function () {
+
+                that._flotrDraw(data[i]);
+                i++;
+
+                if (i < data.length) {
+                    if (timeout) clearTimeout(timeout);
+                    timeout = setTimeout(localDraw, 100);
+                }
+            }
+
+            localDraw();
+        },
+
+        _flotrDraw : function (data) {
+
+            Flotr.draw(
+                this.node,
+                data,
+                {
+                    bubbles : { show : true, baseRadius : 5 },
+                    grid: {outlineWidth: 0, labelMargin: 0},
+                    xaxis   : { min : 0, max : 10, ticks : null, noTicks : 10, tickFormatter : function () { return ''; } },
+                    yaxis   : { min : 0, max : 10, ticks : null, noTicks : 10, tickFormatter : function () { return ''; } }
+                }
+            );
+        }
+    }
+
+    Humble.Bubble = Bubble;
+
+})();
