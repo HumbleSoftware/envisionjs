c0fdb53aa845c983e8d3a8fc206ca7811b52fdc3
Carl Sutherland <carl@humblesoftware.com>
2011-09-12 16:05:50 -0400

    Formatting.

--- dev/weierstrass.js
+++ dev/weierstrass.js
@@ -9,16 +9,21 @@ var count = 1e6,
 //console.time('weierstrass');
 console.log('var weierdata = [');
 
+
 for (i = 1; i < count; i++) {
   x = -1 + i * 2 / count;
   y = 0;
   for (var k = 1; k < 50; k++) {
-    y += Math.sin(Math.PI * Math.pow(k, a) * x) / (Math.PI * Math.pow(k,a)); 
+    y += 
+
+      Math.sin(Math.PI * Math.pow(k, a) * x) / 
+      (Math.PI * Math.pow(k,a)); 
   }
   x = Math.round(x*precision)/precision;
   y = Math.round(y*precision)/precision;
   console.log('[%d, %d],', x, y);
 }
+
 
 // Unfolded final loop (no trailing comma);
 i = count;
