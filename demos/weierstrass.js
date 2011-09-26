2e1f1830bb228c573992664f2296489554cfde0e
Carl Sutherland <carl@humblesoftware.com>
2011-09-26 13:13:12 -0400

    Reworked the weierstrass function for a weierstrass demo - no longer one million points.

--- demos/weierstrass.js
+++ demos/weierstrass.js
@@ -1,80 +1,68 @@
 function example () {
 
   var E = Flotr.EventAdapter,
-    count = 1e6,
-    data = [],
-    progress = document.getElementById('progress'),
     container = document.getElementById('demo'),
     a = 2, // Weierstrass a 
-    step = count / 100,
-    i = 0,
-    j = 0,
-    iMax = count / step,
-    jMax = step * (i + 1),
-    k, x, y, t;
+    count = 600,
+    min = -1,
+    max = 1,
+    data,
+    i, k, x, y, t;
 
-  console.time('data generation');
-  weierstrassStep(i, jMax);
+  function weierstrass (min, max, count) {
 
-  function weierstrassStep (i, jMax) {
-    progress.innerHTML = i;
-    for (j = step * i; j < jMax; j++) {
-      x = -1 + j * 2 / count;
+    var
+      data = [],
+      step = (max - min) / count,
+      x = min,
+      i, j;
+
+    for (i = 0; i <= count; i++, x += step) {
       y = 0;
-      for (k = 1; k < 100; k++) {
-        t = Math.PI * Math.pow(k, a);
+      for (j = 1; j < 100; j++) {
+        t = Math.PI * Math.pow(j, a);
         y += Math.sin(t * x) / t; 
       }
       data.push([x, y]);
     }
-    if (i == iMax - 1) {
-      console.timeEnd('data generation');
-      progress.innerHTML = 100;
-      letsWomp();
-    } else {
-      i = i+1;
-      jMax = step * (i + 1);
-      setTimeout(_.bind(weierstrassStep, null, i, jMax), 2);
+
+    return data;
+  }
+
+  data = weierstrass(min, max, count);
+
+  var H = Humble.Vis,
+    mainOptions = {},
+    summaryOptions = {},
+    v, main, summary, interaction;
+
+  // Config
+  mainOptions.height = 260;
+  mainOptions.width = 600;
+  mainOptions.data  = data;
+  mainOptions.flotr = {};
+
+  summaryOptions.height = 140;
+  summaryOptions.width = 600;
+  summaryOptions.data  = data;
+  summaryOptions.flotr = {
+    handles : {
+      show : true
+    },
+    selection : {
+      mode : 'x'
     }
-  }
+  };
 
-  // womp womp womp
-  function letsWomp () {
+  v = new H.Visualization,
+  main = new H.Child(mainOptions),
+  summary = new H.Child(summaryOptions),
 
-    var H = Humble.Vis,
-      mainOptions = {},
-      summaryOptions = {},
-      v, main, summary, interaction;
+  v.add(main);
+  v.add(summary);
+  v.render(document.getElementById('demo'));
 
-    // Config
-    mainOptions.height = 260;
-    mainOptions.width = 600;
-    mainOptions.data  = data;
-    mainOptions.flotr = {};
-
-    summaryOptions.height = 140;
-    summaryOptions.width = 600;
-    summaryOptions.data  = data;
-    summaryOptions.flotr = {
-      handles : {
-        show : true
-      },
-      selection : {
-        mode : 'x'
-      }
-    };
-
-    v = new H.Visualization,
-    main = new H.Child(mainOptions),
-    summary = new H.Child(summaryOptions),
-
-    v.add(main);
-    v.add(summary);
-    v.render(document.getElementById('demo'));
-
-    interaction = new H.Interaction({leader : summary});
-    interaction.add(H.Action.Selection);
-    interaction.follow(main);
-
-  }
+  interaction = new H.Interaction({leader : summary});
+  interaction.add(H.Action.Selection);
+  interaction.follow(main);
 }
