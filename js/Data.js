bbccfe770501852a7c882e0b2b9ee519cf4e0f05
Carl Sutherland <carl@humblesoftware.com>
2011-10-18 18:54:57 -0400

    Added min-max subsampling.

--- js/Data.js
+++ js/Data.js
@@ -31,6 +31,58 @@ Data.prototype = {
 
     return this;
   },
+  /**
+   * Sample using min and max.
+   */
+  subsampleMinMax : function (resolution) {
+
+    var
+      data    = this.data,
+      length  = data.length,
+      _length = length - 1,
+      newData = [],
+      unit    = resolution / length,
+      index   = Math.round(unit * 1),
+      iMin    = 1,
+      iMax    = 1,
+      min, max, datum, i, j;
+
+    if (length > resolution) {
+      newData.push(data[0]);
+      for (i = 1; i < _length; i++) {
+        j = Math.round(unit * i);
+
+        // New index
+        if (j !== index) {
+          if (iMax === iMin) {
+            newData.push(data[iMin]);
+          } else {
+            newData.push(data[Math.min(iMin, iMax)], data[Math.max(iMin, iMax)]);
+          }
+          iMin = iMax = i;
+          min = max = data[i][1];
+          index = j;
+        }
+
+        // Old index
+        else {
+          datum = data[i][1];
+          if (datum < min) {
+            iMin = i;
+            min = datum;
+          }
+          if (datum > max) {
+            iMax = i;
+            max = datum;
+          }
+        }
+      }
+      newData.push(data[length-1]);
+      this.data = newData;
+    }
+
+    return this;
+  },
 
   subsample : function (resolution) {
 
