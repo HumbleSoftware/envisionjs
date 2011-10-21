3dac02e6ea55229990a47b44e33a6c9924268cb4
Carl Sutherland <carl@humblesoftware.com>
2011-10-21 09:15:08 -0400

    Updated to use dynamic function data generation.

--- demos/weierstrass.js
+++ demos/weierstrass.js
@@ -1,59 +1,69 @@
 function example () {
 
-  var E = Flotr.EventAdapter,
-    container = document.getElementById('demo'),
-    a = 2, // Weierstrass a 
-    count = 600,
-    min = -1,
-    max = 1,
-    data,
-    i, k, x, y, t;
-
-  function weierstrass (min, max, count) {
+  // Weierstrass Function Constructor
+  function weierstrass () {
 
     var
-      data = [],
-      step = (max - min) / count,
-      x = min,
-      i, j;
+      a = 2, // Weierstrass a 
+      i, k, x, y, t;
 
-    for (i = 0; i <= count; i++, x += step) {
-      y = 0;
-      for (j = 1; j < 100; j++) {
-        t = Math.PI * Math.pow(j, a);
-        y += Math.sin(t * x) / t; 
+    function data (min, max, resolution) {
+
+      var
+        d     = [],
+        step  = (max - min) / resolution,
+        x     = min,
+        i, j;
+
+      for (i = 0; i <= resolution; i++, x += step) {
+        y = 0;
+        for (j = 1; j < 100; j++) {
+          t = Math.PI * Math.pow(j, a);
+          y += Math.sin(t * x) / t; 
+        }
+        d.push([x, y]);
       }
-      data.push([x, y]);
+
+      return d;
     }
 
     return data;
   }
 
-  data = weierstrass(min, max, count);
-
-  var H = Humble.Vis,
-    mainOptions = {},
-    summaryOptions = {},
+  var
+    container       = document.getElementById('demo'),
+    H               = Humble.Vis,
+    mainOptions     = {
+      height  : 200,
+      width   : 600,
+      data    : weierstrass(),
+      flotr   : {
+        xaxis : {
+          min : -1,
+          max : 1
+        }
+      }
+    },
+    summaryOptions  = {
+      height  : 200,
+      width   : 600,
+      data    : weierstrass(),
+      flotr : {
+        handles : {
+          show : true
+        },
+        selection : {
+          mode : 'x'
+        },
+        xaxis : {
+          min : -1,
+          max : 1
+        }
+      }
+    },
     v, main, summary, interaction;
 
-  // Config
-  mainOptions.height = 260;
-  mainOptions.width = 600;
-  mainOptions.data  = data;
-  mainOptions.flotr = {};
-
-  summaryOptions.height = 140;
-  summaryOptions.width = 600;
-  summaryOptions.data  = data;
-  summaryOptions.flotr = {
-    handles : {
-      show : true
-    },
-    selection : {
-      mode : 'x'
-    }
-  };
-
+  // Visualization
   v = new H.Visualization,
   main = new H.Child(mainOptions),
   summary = new H.Child(summaryOptions),
