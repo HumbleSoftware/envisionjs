dae5548235e32769db2b02b2153faa4e494093d5
Carl Sutherland <carl@humblesoftware.com>
2012-02-07 17:03:31 -0500

    Added lite-lines, a fast barebones implementation of lines.

---
+++ js/flotr/lite-lines.js
@@ -0,0 +1,71 @@
+/** Lines **/
+Flotr.addType('lite-lines', {
+  options: {
+    show: false,           // => setting to true will show lines, false will hide
+    lineWidth: 2,          // => line width in pixels
+    fill: false,           // => true to fill the area from the line to the x axis, false for (transparent) no fill
+    fillBorder: false,     // => draw a border around the fill
+    fillColor: null,       // => fill color
+    fillOpacity: 0.4,      // => opacity of the fill color, set to 1 for a solid fill, 0 hides the fill
+  },
+
+  /**
+   * Draws lines series in the canvas element.
+   * @param {Object} options
+   */
+  draw : function (options) {
+
+    var
+      context     = options.context,
+      lineWidth   = options.lineWidth,
+      shadowSize  = options.shadowSize,
+      offset;
+
+    context.save();
+    context.lineCap = 'butt';
+    context.lineWidth = lineWidth;
+    context.strokeStyle = options.color;
+
+    this.plot(options);
+
+    context.restore();
+  },
+
+  plot : function (options) {
+
+    var
+      context   = options.context,
+      xScale    = options.xScale,
+      yScale    = options.yScale,
+      data      = options.data, 
+      length    = data.length - 1,
+      x0        = xScale(data[0][0]),
+      y0        = yScale(data[0][1]),
+      zero      = yScale(0);
+      
+    if (length < 1) return;
+
+    context.beginPath();
+    context.moveTo(x0, y0);
+    for (i = 0; i < length; ++i) {
+      context.lineTo(
+        xScale(data[i+1][0]),
+        yScale(data[i+1][1])
+      );
+    }
+
+    if (!options.fill || options.fill && !options.fillBorder) context.stroke();
+
+    if (options.fill){
+      x0 = xScale(data[0][0]);
+      context.fillStyle = options.fillStyle;
+      context.lineTo(xScale(data[length][0]), zero);
+      context.lineTo(x0, zero);
+      context.lineTo(x0, yScale(data[0][1]));
+      context.fill();
+      if (options.fillBorder) {
+        context.stroke();
+      }
+    }
+  }
+});
