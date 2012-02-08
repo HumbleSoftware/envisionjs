5ee54109dc7e7fff5d8d1e7f0469fe141935931c
Carl Sutherland <carl@humblesoftware.com>
2012-02-08 10:53:11 -0500

    Added validation spec.

--- spec/js/Preprocessor.js
+++ spec/js/Preprocessor.js
@@ -12,7 +12,7 @@ describe('Preprocessor', function () {
 
   it('gets private data from options', function () {
     var
-      data = [],
+      data = [[],[]],
       options = { data : data },
       preprocessor = new H.Preprocessor(options);
 
@@ -20,13 +20,49 @@ describe('Preprocessor', function () {
   });
 
   it('sets new private data', function () {
+
     var
-      data = [],
-      options = { data : [] },
+      data = [[],[]],
+      options = { data : [[], []] },
       preprocessor = new H.Preprocessor(options);
 
     expect(preprocessor.getData()).not.toBe(data);
     preprocessor.setData(data);
     expect(preprocessor.getData()).toBe(data);
+  });
+
+  describe('Validation', function () {
+
+    var
+      preprocessor;
+
+    beforeEach(function () {
+      preprocessor = new H.Preprocessor();
+    });
+    afterEach(function () {
+      preprocessor = null;
+    });
+
+    it('expects array data', function () {
+      expect(function () { preprocessor.setData({}); })
+        .toThrow(new Error("Array expected."));
+    });
+
+    it('expects at least two dimensions', function () {
+      var
+        e = new Error('Data must contain at least two dimensions.');
+      expect(function () { preprocessor.setData([]); }).toThrow(e);
+      expect(function () { preprocessor.setData([[]]); }).toThrow(e);
+    });
+
+    it('expects each dimension to be an array', function () {
+      expect(function () { preprocessor.setData([[], {}]); })
+        .toThrow(new Error('Data dimensions must be arrays.'));
+    });
+
+    it('expects each dimension to have the same number of points', function () {
+      expect(function () { preprocessor.setData([[], [1]]); })
+        .toThrow(new Error('Data dimensions must contain the same number of points.'));
+    });
   });
 });
