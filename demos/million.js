e318273cd960a033c8bd12839fb8f14a5b08ed1b
Carl Sutherland <carl@humblesoftware.com>
2012-02-08 09:40:56 -0500

    Updated for new data interface.

--- demos/million.js
+++ demos/million.js
@@ -3,19 +3,21 @@ function example () {
   var
     container = document.getElementById('demo'),
     E = Flotr.EventAdapter,
-    data = [],
+    data = { x : [], y : [] },
     hash = window.location.hash,
     amplitude = (hash ? .05 : .001),
     summary = {
       data : data
     },
     zoom = {
-      data : data
+      data : [ data ]
     },
     million;
 
+  data.length = 1e6;
   for (i = 0; i < 1e6; i++) {
-    data.push([i/10000, .2*Math.sin(i/10000) + i/100000 + amplitude * Math.sin(i/50)]);
+    data.x.push(i/10000);
+    data.y.push(.2*Math.sin(i/10000) + i/100000 + amplitude * Math.sin(i/50));
   }
 
   if (hash !== '#minmax') {
