5e2bb2e3fa07b6e52aeb995cd79e0c65d1259bc8
Carl Sutherland <carl@humblesoftware.com>
2012-03-07 09:55:59 -0500

    Added flashcanvas.

--- demos/includes.build.js
+++ demos/includes.build.js
@@ -1,4 +1,21 @@
+window.FlashCanvasOptions = {
+  swfPath: 'lib/FlashCanvas/bin/'
+};
 yepnope([
+
+  {
+    test : (navigator.appVersion.indexOf("MSIE") != -1  && parseFloat(navigator.appVersion.split("MSIE")[1]) < 9),
+    yep : [
+      'lib/flotr2/lib/base64.js'
+    ]
+  },
+  {
+    test : (navigator.appVersion.indexOf("MSIE") != -1),
+    yep : [
+      'lib/FlashCanvas/bin/flashcanvas.js'
+    ]
+  },
+
   // Libs
   'flotr2.min.js',
   'handles.js',
