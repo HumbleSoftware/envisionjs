ed564ca922e19f5c1cd01d57326e636fca342e9f
Carl Sutherland <carl@humblesoftware.com>
2012-03-08 15:23:24 -0500

    Initial finance template.

---
+++ js/templates/Finance.js
@@ -0,0 +1,161 @@
+(function () {
+
+var
+  V = envision;
+
+// Custom data processor
+function processData (options) {
+
+  var
+    resolution = options.resolution;
+
+  options.preprocessor
+    .bound(options.min, options.max)
+    .subsampleMinMax(resolution + Math.round(resolution / 3));
+}
+
+function getDefaults () {
+  return {
+    price : {
+      name : 'price',
+      //data : D.price,
+      flotr : {
+        'lite-lines' : {
+          lineWidth : 1,
+          show : true,
+          fill : true,
+          fillOpacity : .2
+        },
+        mouse : {
+          track: true,
+          trackY: false,
+          trackAll: true,
+          sensibility: 1,
+          trackDecimals: 4,
+          trackFormatter: function (o) {
+            var data = jsonData[o.nearest.x];
+            return data.date + " Price: " + data.close + " Vol: " + data.volume;
+          },
+          position: 'ne'
+        },
+        yaxis : { 
+          noTicks : 3,
+          showLabels : true,
+          min : 0,
+          tickFormatter : function (n) {
+            return (n == this.max ? false : '$'+n);
+          }
+        }
+      },
+      processData : processData
+    },
+    volume : {
+      name : 'volume',
+      //data : D.volume,
+      flotr : {
+        whiskers : {
+          show : true 
+        },
+        mouse: {
+          track: true,
+          trackY: false,
+          trackAll: true,
+          position: 'ne',
+          trackDecimals: 0
+        }
+      },
+      processData : processData
+    },
+    summary : {
+      name : 'summary',
+      //data : D.price,
+      flotr : {
+        'lite-lines' : {
+          show : true,
+          lineWidth : 1,
+          fill : true,
+          fillOpacity : .2,
+          fillBorder : true
+        },
+        xaxis : {
+          noTicks: 5,
+          showLabels : true,
+          tickFormatter : function (n) {
+            return jsonData[n].date.split(' ')[2];
+          }
+        },
+        yaxis : {
+          autoscale : true,
+          autoscaleMargin : .1
+        },
+        handles : {
+          show : true
+        },
+        selection : {
+          mode : 'x'
+        },
+        grid : {
+          verticalLines : false
+        }
+      }
+    },
+    connection : {
+      name : 'connection',
+      drawing : V.QuadraticDrawing
+    }
+  };
+}
+
+function Finance (options) {
+
+  var
+    data = options.data,
+    defaults = getDefaults(),
+    vis = new V.Visualization(),
+    selection = new V.Interaction(),
+    hit = new V.Interaction(),
+    price, volume, connection, summary;
+
+  defaults.price.data = data.price;
+  defaults.volume.data = data.volume;
+  defaults.summary.data = data.summary;
+  price = new V.Child(defaults.price),
+  volume = new V.Child(defaults.volume),
+  connection = new V.Child(defaults.connection),
+  summary = new V.Child(defaults.summary),
+
+  // Render visualization
+  vis
+    .add(price)
+    .add(volume)
+    .add(connection)
+    .add(summary)
+    .render(container);
+
+  // Define the selection zooming interaction
+  selection
+    .follower(price)
+    .follower(volume)
+    .follower(connection)
+    .leader(summary)
+    .add(V.action.selection);
+
+  // Define the mouseover hit interaction
+  hit
+    .group([price, volume])
+    .add(V.action.hit);
+
+  // Optional initial selection
+  if (options.selection) {
+    summary.trigger(summary, 'select', options.selection);
+  }
+
+  // Members
+  this.vis = vis;
+  this.selection = selection;
+  this.hit = hit;
+}
+
+V.templates.Finance = Finance;
+
+})();
