17db969a31d9ba73702568d488b3b812f41cad2b
Carl Sutherland <carl@humblesoftware.com>
2012-03-12 14:21:18 -0400

    Converted to timeseries.

--- demos/million.js
+++ demos/million.js
@@ -1,51 +1,55 @@
 function example () {
 
   var
+    hash = window.location.hash,
     container = document.getElementById('demo'),
-    E = Flotr.EventAdapter,
+    amplitude = (hash ? .05 : .0015),
     x = [],
     y = [],
     data = [x, y],
-    hash = window.location.hash,
-    amplitude = (hash ? .05 : .0015),
-    summary = {
-      data : data,
-      flotr : {
-        yaxis : {
-          autoscale : true,
-          autoscaleMargin : 1
-        }
-      }
-    },
-    zoom = {
-      data : [ data ]
-    },
+    detail = {},
+    summary = {},
+    options,
     million;
 
+  // Data Generation
   for (i = 0; i < 1e6; i++) {
     x.push(i/10000);
     y.push(Math.sin(i/50000) + amplitude * Math.sin(i/25));
   }
 
+  // Subsampling Demos
   if (hash !== '#minmax') {
+
     // Fixed-interval subsampling:
-    zoom.processData = summary.processData = function (o) {
+    detail.processData = summary.processData = function (o) {
       o.preprocessor
         .bound(o.min, o.max)
-        .subsample(1000);//o.resolution);
+        .subsample(1000);
     }
   } else {
-    zoom.processData = summary.processData = function (o) {
+
+    // Min-max subsampling:
+    detail.processData = summary.processData = function (o) {
       o.preprocessor
         .bound(o.min, o.max)
         .subsampleMinMax(o.resolution * 1.5);
     }
   }
 
-  // Zoom template:
-  million = new envision.templates.Zoom({
+  // Options
+  options = {
     container : container,
-    summary : summary,
-    zoom : zoom
-  });
+    data : {
+      detail : data,
+      summary : data
+    },
+    defaults : {
+      detail : detail,
+      summary : summary
+    }
+  };
+
+  // TimeSeries template:
+  million = new envision.templates.TimeSeries(options);
 }
