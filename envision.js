aa6701c3e04591561c5ffddedb0e04ad12347184
Carl Sutherland <carl@humblesoftware.com>
2012-03-13 12:16:36 -0400

    Abgebaut.

--- envision.js
+++ envision.js
@@ -1,3 +1,9 @@
+// Envision.js
+// (c) 2012 Carl Sutherland, Humble Software
+// Distributed under the MIT License
+// Source: http://www.github.com/HumbleSoftware/envisionjs
+// Homepage: http://www.humblesoftware.com/envision
+
 envision = {};
 
 (function () { 
