c0c73f3fbaa12dfb91a0ffbd14130e40b9dbec89
Carl Sutherland <carl@humblesoftware.com>
2012-03-13 11:29:41 -0400

    Added license.

---
+++ js/license.js
@@ -0,0 +1,5 @@
+// Envision.js
+// (c) 2012 Carl Sutherland, Humble Software
+// Distributed under the MIT License
+// Source: http://www.github.com/HumbleSoftware/envisionjs
+// Homepage: http://www.humblesoftware.com/envision
