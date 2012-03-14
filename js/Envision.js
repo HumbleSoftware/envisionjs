881748a9a1eab7f1a864c82d5a3e62286f17b853
Carl Sutherland <carl@humblesoftware.com>
2012-03-14 14:42:41 -0400

    Added noConflict.  Not sure what people want as far as the deps go.  Call no conflict individually or maybe the lib should do it for them?

--- js/Envision.js
+++ js/Envision.js
@@ -6,5 +6,14 @@ envision = {
   // Globals
   _ : Flotr._,        // Underscore.js, functional microlib
   bean : Flotr.bean,  // Bean, events
-  bonzo : bonzo       // Bonzo, dom
+  bonzo : bonzo,      // Bonzo, dom
+
+  // Utility
+  noConflict : (function (root) {
+    var previous = root.envision;
+    return function () {
+      root.envision = previous;
+      return this;
+    }
+  })(this)
 };
--- spec/js/Envision.js
+++ spec/js/Envision.js
@@ -23,4 +23,16 @@ describe('Envision', function () {
 
   });
 
+  describe('Utility', function () {
+
+    it('has a noConflict', function () {
+      var root = (function () { return this; })(); // Not strict compatible?
+      expect(root.envision).toBe(E);
+      expect(E.noConflict).toBeDefined();
+      expect(E.noConflict()).toBe(E);
+      expect(root.envision).not.toBe(E);
+    });
+
+  });
+
 });
