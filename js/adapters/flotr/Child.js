b8e306c34506e697fcf8f569c7ab979500c532a6
Carl Sutherland <carl@humblesoftware.com>
2012-07-01 13:38:27 -0400

    Add check for empty series.
    We allow the passing of a single series directly but Flotr2 does not.
    The translation from single series to array of one series wasn't
    properly handling the case of an initial series with no elements in it.
    For #6.

--- js/adapters/flotr/Child.js
+++ js/adapters/flotr/Child.js
@@ -73,7 +73,14 @@ Child.prototype = {
 
   getDataArray : function (data) {
 
-    if (data[0] && (!_.isArray(data[0]) || (data[0][0] && _.isArray(data[0][0]))))
+    if (
+      data[0] && // Data exists and
+      (
+        !_.isArray(data[0]) || // data not an array
+        !data[0].length || // data is an empty series
+        (data[0][0] && _.isArray(data[0][0])) // data is a series
+      )
+    )
       return data;
     else
       return [data];
