60dd3b22851c60346e7a426818a6c67f04b4c42a
Carl Sutherland <carl@humblesoftware.com>
2012-07-11 19:22:29 -0400

    Fix for Flotr2 selection area API updates.

--- js/adapters/flotr/Child.js
+++ js/adapters/flotr/Child.js
@@ -199,23 +199,23 @@ Child.prototype = {
         if (!x && data.x) {
           // Translate data to pixels
           x = data.x;
-          options.x1 = axes.x.d2p(x.min);
-          options.x2 = axes.x.d2p(x.max);
-        } else if (x) {
-          // Use pixels
           options.x1 = x.min;
           options.x2 = x.max;
+        } else if (x) {
+          // Use pixels
+          options.x1 = axes.x.p2d(x.min);
+          options.x2 = axes.x.p2d(x.max);
         }
 
         if (!y && data.y) {
           // Translate data to pixels
           y = data.y;
-          options.y1 = axes.y.d2p(y.min);
-          options.y2 = axes.y.d2p(y.max);
-        } else if (y) {
-          // Use pixels
           options.y1 = y.min;
           options.y2 = y.max;
+        } else if (y) {
+          // Use pixels
+          options.y1 = axes.y.d2p(y.min);
+          options.y2 = axes.y.d2p(y.max);
         }
 
         graph.selection.setSelection(options);
